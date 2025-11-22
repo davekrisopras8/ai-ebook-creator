3ad6dcee103d09e86e5ed30cd235d88a6f3778a3
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-11-22 14:10:12 +0700

    feat: create project structure, file and folder

---
+++ frontend/src/components/view/ViewChapterSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewChapterSidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewChapterSidebar
---
+++ frontend/src/pages/ViewBookPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewBookPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewBookPage
