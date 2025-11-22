d7a0ad56a5061c87f093b93f5727c56c100af955
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-11-22 16:46:47 +0700

    feat: defining app routes

--- frontend/src/components/auth/ProtectedRoute.jsx
+++ frontend/src/components/auth/ProtectedRoute.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ProtectedRoute = ({children}) => {
+  const isAuthenticated = false;
+  const loading = false;
+  const location = useLocation();
 
-export default ProtectedRoute
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
