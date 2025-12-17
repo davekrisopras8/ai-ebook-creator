0f7347a9be1b08cb255eeee12832845188c7c98b
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-12-17 20:55:58 +0700

    feat: login page

--- frontend/src/components/auth/ProtectedRoute.jsx
+++ frontend/src/components/auth/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
-const ProtectedRoute = ({children}) => {
-  const isAuthenticated = false;
-  const loading = false;
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
   const location = useLocation();
 
   if (loading) {
